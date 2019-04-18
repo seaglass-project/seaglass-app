2bcf0bdb077aec5f06db0f3271047c901d9f7205
Karl Koscher <supersat@cs.washington.edu>
2019-04-18 13:41:19 -0700

    Collect GSMPacket payloads in server endpoint

--- server/index.php
+++ server/index.php
@@ -80,6 +80,7 @@ foreach ($data['gsmPackets'] as $gsmPacket) {
     $subtype = $gsmPacket['subtype'];
     $timeslot = $gsmPacket['timeslot'];
     $frameNumber = $gsmPacket['frameNumber'];
+    $payload = $gsmPacket['payload'];
     $timestamp = $gsmPacket['measurementHeader']['timestamp'];
     $band = $gsmPacket['measurementHeader']['band'];
     $arfcn = $gsmPacket['measurementHeader']['arfcn'];
